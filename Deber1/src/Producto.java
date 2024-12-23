f2f92156382c8aa01c8a450a34e26480e5f49647
luis <luis.davila@tecnologicoliceocristiano.edu.ec>
2024-12-23 03:47:01 -0500

    Mi primer commit

---
+++ Deber1/src/Producto.java
@@ -0,0 +1,28 @@
+
+public class Producto {
+
+	public static void main(String[] args) {
+		System.out.println("Producto");
+        System.out.println("Categoría");
+        System.out.println("Precio");
+        System.out.println("Cantidad en stock");
+        System.out.println("Peso");
+        System.out.println("Color");
+        System.out.println("Marca");
+        System.out.println("Código de barras");
+        System.out.println("Disponible");
+        System.out.println("Descripción");
+        System.out.println("Proveedor");
+        System.out.println("Fecha de fabricación");
+        System.out.println("Fecha de vencimiento:");
+        System.out.println("Tamaño");
+        System.out.println("Altura");
+        System.out.println("Largo:");
+        System.out.println("Ancho:");
+        System.out.println("Envío gratis");
+        System.out.println("En oferta");
+        System.out.println("Ubicación en almacen");
+
+	}
+
+}
